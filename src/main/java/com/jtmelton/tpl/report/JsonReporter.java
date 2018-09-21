c736b7d72971b3167c326f8934a511c546232e82
Armando Sosa <armandojosuesosa@gmail.com>
2018-09-20 19:44:12 -0500

    Converted to gson json stream writer

--- src/main/java/com/jtmelton/tpl/report/JsonReporter.java
+++ src/main/java/com/jtmelton/tpl/report/JsonReporter.java
@@ -1,81 +1,88 @@
 package com.jtmelton.tpl.report;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.google.gson.stream.JsonWriter;
 
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
 public class JsonReporter implements IReporter {
 
-  private static final Logger LOG = LoggerFactory.getLogger(JsonReporter.class);
+  private JsonWriter writer;
 
-  private JSONObject jsonRoot = new JSONObject();
-
-  private JSONArray relationships = new JSONArray();
-
-  private JSONArray currentChains;
-
-  private JSONObject currentChainEntry;
-
-  public JsonReporter() {
-    jsonRoot.put("DependencyRelationships", relationships);
-  }
-
-  @Override
-  public void preProcess(Collection<String> jars) {
-    JSONObject matchedJar = new JSONObject();
-    currentChains = new JSONArray();
-
-    matchedJar.put("chains", currentChains);
-    matchedJar.put("jars", jars);
-
-    relationships.put(matchedJar);
-  }
-
-  @Override
-  public void chainEntryStart() {
-    currentChainEntry = new JSONObject();
-    JSONArray chain = new JSONArray();
-    currentChainEntry.put("chain", chain);
-  }
-
-  @Override
-  public void chainEntryEnd() {
-    currentChains.put(currentChainEntry);
-  }
-
-  @Override
-  public void addChainEntryUserClass(String className) {
-    currentChainEntry.put("userClass", className);
-  }
-
-  @Override
-  public void addChainLink(String className, Collection<String> jars) {
-    JSONObject link = new JSONObject();
-    link.put("class", className);
-    link.put("jars", jars);
-
-    currentChainEntry.getJSONArray("chain").put(link);
-  }
-
-  @Override
-  public void report(String outputFile) {
+  public JsonReporter(String outputFile) throws IOException {
     Path outputPath = Paths.get(outputFile + ".json");
+    FileWriter fw = new FileWriter(outputPath.toFile());
 
-    String report = jsonRoot.toString(2);
+    writer = new JsonWriter(fw);
+    writer.setIndent("  ");
+    writer.beginObject();
+    writer.name("DependencyRelationships");
+    writer.beginArray();
+  }
 
-    LOG.info("Writing JSON results: {}", outputPath.toAbsolutePath());
+  @Override
+  public void preProcess(String searchTerm, Collection<String> jars) throws IOException {
+      writer.beginObject();
 
-    try {
-      Files.write(outputPath, report.getBytes());
-    } catch(IOException ioe) {
-      LOG.warn("Failed to write JSON results", ioe);
+      writer.name("jars");
+
+      writer.beginArray();
+      for(String jar : jars) {
+        writer.value(jar);
+      }
+      writer.endArray();
+
+      writer.name("chains");
+
+      writer.beginArray();
+  }
+
+  @Override
+  public void chainEntryStart() throws IOException {
+    writer.beginObject();
+  }
+
+  @Override
+  public void chainEntryEnd() throws IOException {
+    writer.endArray();
+    writer.endObject();
+  }
+
+  @Override
+  public void addChainEntryUserClass(String className) throws IOException {
+    writer.name("userClass").value(className);
+    writer.name("chain");
+    writer.beginArray();
+  }
+
+  @Override
+  public void addChainLink(String className, Collection<String> jars) throws IOException {
+    writer.beginObject();
+
+    writer.name("class").value(className);
+
+    writer.name("jars");
+    writer.beginArray();
+    for (String jar : jars) {
+      writer.value(jar);
     }
+    writer.endArray();
+
+    writer.endObject();
+  }
+
+  @Override
+  public void endProcess() throws IOException {
+    writer.endArray();
+    writer.endObject();
+  }
+
+  @Override
+  public void report(String outputFile) throws IOException {
+    writer.endArray();
+    writer.endObject();
+    writer.close();
   }
 }
