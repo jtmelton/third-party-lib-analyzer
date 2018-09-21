2ac074385176fbbd3d21bd01994b11c37d5eab6b
Armando Sosa <armandojosuesosa@gmail.com>
2018-09-20 19:42:19 -0500

    Added endProcess interface method. Also added IOExceptions being thrown by methods to facilitate buffered writing as results are being read

--- src/main/java/com/jtmelton/tpl/report/IReporter.java
+++ src/main/java/com/jtmelton/tpl/report/IReporter.java
@@ -1,12 +1,14 @@
 package com.jtmelton.tpl.report;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface IReporter {
-  void preProcess(Collection<String> jars);
-  void chainEntryStart();
-  void chainEntryEnd();
-  void addChainEntryUserClass(String className);
-  void addChainLink(String className, Collection<String> jars);
-  void report(String outputFile);
+  void preProcess(String searchTerm, Collection<String> jars) throws IOException;
+  void endProcess() throws IOException;
+  void chainEntryStart() throws IOException;
+  void chainEntryEnd() throws IOException;
+  void addChainEntryUserClass(String className) throws IOException;
+  void addChainLink(String className, Collection<String> jars) throws IOException;
+  void report(String outputFile) throws IOException;
 }
